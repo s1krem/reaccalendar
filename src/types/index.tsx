73efaeee887dabb2f481deaf55c6c0b64c7ac25f
merkys <mrkmerkys@gmail.com>
2025-03-16 17:10:34 +0200

    initial

---
+++ src/types/index.tsx
@@ -0,0 +1,21 @@
+
+
+export interface Reminder {
+    id?: number;
+    title: string;
+    description: string;
+    startTime: string;
+    recurrence?: string | null;
+    recurrenceEndTime?: string | null;
+    createdDate?: string;
+    updatedDate?: string;
+  }
+  
+  export interface Holiday {
+    date: string;       
+    localName: string;  
+    name: string;       
+    countryCode?: string;
+    global?: boolean;
+  }
+  
